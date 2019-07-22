b104240d29962beb627e41a268f165caf396a1cd
Derek S <dschwan21@me.com>
2019-07-22 14:14:50 -0700

    MVP

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,12 +1,47 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
-// const ProjectDb = require("../helpers/projectModel");
+const Users = require("./mainHelpers");
+const authenticate = require("../middleware/auth-middleware");
 
 const router = express.Router();
 
-router.get("/users", (req, res) => {});
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-router.post("/register", (req, res) => {});
+router.get("/users", authenticate, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json({ err: "something went wrong" }));
+});
 
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 module.exports = router;
